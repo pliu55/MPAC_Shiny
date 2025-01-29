7b363f47677fda2e00ac3b69fda5fdf480186635
pliu55 <pliu55@wisc.edu>
2025-01-28 18:02:07 -0600

    re-index the 4 Boxes

--- body.R
+++ body.R
@@ -1,5 +1,5 @@
 body_tcga_pathway = fluidRow(
-    box(title = '(A) Pathway over-representation in TCGA patient samples',
+    box(title = '(i) Pathway over-representation in TCGA patient samples',
         width=12, collapsible=TRUE,
         selectizeInput( inputId  = 'tcga_ovr_gonames',
                         label    = 'Enter pathway name(s):',
@@ -8,7 +8,7 @@ body_tcga_pathway = fluidRow(
         uiOutput('tcga_ovr')
     ),
 
-    box(title = paste0('(B) Inferred pathway levels (IPLs) of proteins in the ',
+    box(title =paste0('(ii) Inferred pathway levels (IPLs) of proteins in the ',
                        'selected pathway below'),
         width=12, collapsible=TRUE,
         selectizeInput( inputId  = 'tcga_ipl_goname',
@@ -20,23 +20,23 @@ body_tcga_pathway = fluidRow(
 )
 
 body_tcga_protein = fluidRow(
-    box(title = "(C) Features of protein groups",
-        width=12, collapsible=TRUE, 
+    box(title = "(iii) Features of protein groups",
+        width=12, collapsible=TRUE,
         selectizeInput( inputId  = 'tcga_prots',
                         label    = 'Enter protein name(s):',
                         choice   = NULL,
                         multiple = TRUE ),
 
         tabBox(title=h4('Click tab name for figures'), width=12, side='left',
-            tabPanel('Protein inferred pathway levels (IPLs)',   
+            tabPanel('Protein inferred pathway levels (IPLs)',
                    # plotOutput('tcga_prot_ipl')),
                      uiOutput('tcga_prot_ipl')),
             tabPanel('Clinical relevance',       plotOutput('tcga_prot_clin')),
             tabPanel('Immune cell infiltration', plotOutput('tcga_prot_infil')))
     ),
 
-    box(title = "(D) A protein's pathway neighbors and states",
-        width=12, collapsible=TRUE, 
+    box(title = "(iv) A protein's pathway neighbors and states",
+        width=12, collapsible=TRUE,
         selectizeInput( inputId  = 'tcga_nei_stt_prot',
                         label    = 'Enter a protein name:',
                         choice   = NULL,
@@ -47,21 +47,21 @@ body_tcga_protein = fluidRow(
 )
 
 body_acknowledgement = fluidRow(
-    box(title = "", width=12, collapsible=TRUE, 
+    box(title = "", width=12, collapsible=TRUE,
         htmltools::includeMarkdown('ack.md') )
 )
 
 body_futuredev = fluidRow(
-    box(title = "", width=12, collapsible=TRUE, 
+    box(title = "", width=12, collapsible=TRUE,
         htmltools::includeMarkdown('todo.md') )
 )
 
 body_manufig = fluidRow(
-    box(title = "", width=12, collapsible=TRUE, 
+    box(title = "", width=12, collapsible=TRUE,
         htmltools::includeMarkdown('fig.md') )
 )
 
 body_references = fluidRow(
-    box(title = "", width=12, collapsible=TRUE, 
+    box(title = "", width=12, collapsible=TRUE,
         htmltools::includeMarkdown('ref.md') )
 )
